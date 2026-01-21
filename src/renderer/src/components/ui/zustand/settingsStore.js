ac16850336c3c008a8fc6b84b797a9ca91a41f0a
Bradly Chauke <bradlychauke@gmail.com>
2026-01-21 11:02:40 +0200

    Fetched all the necessary data from the preload

--- src/renderer/src/components/ui/zustand/settingsStore.js
+++ src/renderer/src/components/ui/zustand/settingsStore.js
@@ -1,5 +1,19 @@
 import { create } from "zustand";
 
+
+
+function getPreview(format) {
+  const now = new Date();
+  switch (format) {
+    case "dd MMM yy":
+      return now.toLocaleDateString("en-GB", { day: "2-digit", month: "short", year: "2-digit" });
+    case "dd MMMM yyyy":
+      return now.toLocaleDateString("en-GB", { day: "2-digit", month: "long", year: "numeric" });
+    default:
+      return "";
+  }
+}
+
 export const useStatusStore = create ((set) => ({
 
      isAdmin:false,
@@ -12,6 +26,12 @@ export const useStatusStore = create ((set) => ({
       shortDate:null,
       longDate:null,
       lastRead:null,
+
+      //Desired settings states
+      desiredShortDate:null,
+      desiredLongDate:null,
+       shortPrev:null,
+        longPrev:null,
      
      loadAdminStatus: async () =>{
         set({loading:true, error:null})
@@ -34,21 +54,37 @@ export const useStatusStore = create ((set) => ({
                 }catch(error){
                     set({loading:false})
                 }
-
-
      },
 
      loadCurrentDateSettings: async () =>{
                         set({loading:true, error:null})
                         try{
-                            const currentSettings = await window.api. getCurrentSettings()
+                            const currentSettings = await window.api.getCurrentSettings()
                              set({shortDate:currentSettings.shortDate,
-                                 longDate:currentSettings.longDate,
-                                  lastRead: currentSettings.lastRead})
+                                longDate:currentSettings.longDate,
+                                lastRead: currentSettings.lastRead})
                         }catch{
                             set({loading:false})
                         }
 
+     },
+
+     loadDesiredSettings: async () =>{
+         set({loading:true, error:null})
+           try{
+                 const desiredSettings = await window.api.getDesiredSettings()
+                 console.log(desiredSettings)
+                    set({
+                        desiredShortDate:desiredSettings.formats.shortDate,
+                        desiredLongDate:desiredSettings.formats.longDate,
+                           shortPrev: getPreview(desiredSettings.formats.shortDate),
+                          longPrev: getPreview(desiredSettings.formats.longDate),
+                       
+                    })
+           }catch(error){
+                   set({loading:false})
+     }
+             
      }
 
 }))

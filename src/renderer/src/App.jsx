af6ccab538afe54ae9ec5a439cd9b9c2ced9fb72
Chrisnerg <chrisnergmqobo@gmail.com>
2026-01-21 08:19:48 +0200

    Removed testing of backend fetching from App.jsx

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TopBar from "./components/ui/TopBar";
 import StatusBar from "./components/ui/StatusBar";
 import CurrentSettings from "./components/ui/CurrentSettings";
 import DesiredSettings from "./components/ui/DesiredSettings";
 import ActionButtons from "./components/ui/ActionButtons";
 const App = () => {
-  const [settings, setSettings] = useState({});
-
-  useEffect( () => {
-    const getSettings = async () => {
-      try { 
-        // const result = await window.api.applySettings();
-        const result = await window.api.getDesiredSettings();
-        setSettings(result);
-        console.log(result);
-      } catch(error) {
-        console.error(error);
-      }
-    };
-
-    getSettings();
-  }, [])
   return (
     <div className="min-h-screen text-text bg-background-light">
       <TopBar />

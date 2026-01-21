e59c0b62c7b13c58e83e0f7690db9af2411a25b0
Bradly Chauke <bradlychauke@gmail.com>
2026-01-21 08:53:32 +0200

    Pulled all backend changes from backend-dev

---
+++ src/main/globals.js
@@ -0,0 +1,11 @@
+const tontracSettings = {
+    formats: {
+        shortDate: "dd MMM yy",
+        longDate: "dd MMMM yyyy",
+        shortTime: "HH:mm",
+        longTime: "HH:mm:ss",
+        decimal: ",",
+    },
+};
+
+export default tontracSettings;
--- src/main/ipcMainHandler.js
+++ src/main/ipcMainHandler.js
@@ -1,6 +1,10 @@
 import { ipcMain } from 'electron';
 import { execSync } from 'child_process';
 import os from 'os'
+import tontracSettings from './globals.js';
+import fs from 'fs';
+import { app } from 'electron';
+import path from 'path';
 
 //Get Operating system info
 ipcMain.handle('get-os-info', () => {
@@ -19,13 +23,13 @@ ipcMain.handle('get-os-info', () => {
       operatingSystemType,
       operatingSystemVersion: getWindowsVersion(),
     }
-  } catch {
-    return false;
+  } catch(error) {
+      consol.error("failed to get OS info",error)
   }
 });
 
-//Get current settings from the registry
-ipcMain.handle('get-current-settings', () => {
+//Helper Function - getting current system settings
+const getCurrentRegistrySettings = () => {
     const getRegistryValue = (valueName) => {
       const output = execSync(
         `reg query "HKCU\\Control Panel\\International" /v ${valueName}`
@@ -42,4 +46,66 @@ ipcMain.handle('get-current-settings', () => {
         longTime: getRegistryValue('sTimeFormat'),
         decimal: getRegistryValue('sDecimal'),
     };
+};
+
+//Get current settings from the registry
+ipcMain.handle('get-current-settings', () => {
+  try {
+    return getCurrentRegistrySettings();
+  } catch(error) {
+      console.error("failed to get get current settings", error)
+  }
 });
+
+//Helper function - writing to registry
+const applySettings = (valueName, value) => {
+  execSync(
+    `reg add "HKCU\\Control Panel\\International" /v ${valueName} /t REG_SZ /d "${value}" /f`
+  );
+};
+
+//Helper Function - create backup
+const createBackUp = (currentSettings) => {
+  const appDataPath = app.getPath('appData');
+
+  const backupDir = path.join(appDataPath, 'DateFormatConfigurator');
+
+  if(!fs.existsSync(backupDir)) {
+    fs.mkdirSync(backupDir, { recursive: true })
+  };
+
+  const backupFilePath = path.join(backupDir, 'backup.json');
+
+  fs.writeFileSync(backupFilePath, JSON.stringify(currentSettings, null, 2));
+};
+
+//Write to registry desired settings
+ipcMain.handle('apply-settings', () => {
+  try {
+
+    const currentSettings = getCurrentRegistrySettings();
+
+    const backup = {
+      timestamp: new Date().toISOString(),
+      settings: currentSettings
+    }
+
+    createBackUp(backup);
+
+    applySettings('sShortDate', tontracSettings.formats.shortDate);
+    applySettings('sLongDate', tontracSettings.formats.longDate);
+    applySettings('sShortTime', tontracSettings.formats.shortTime);
+    applySettings('sTimeFormat', tontracSettings.formats.longTime);
+    applySettings('sDecimal', tontracSettings.formats.decimal);
+
+    return { success: true, message: 'Settings applied successfully' };
+  } catch(error) {
+    console.error('Failed to apply settings');
+    return { success: false, message: error.message };
+  }
+});
+
+ipcMain.handle('get-desired-settings', () => {
+  return tontracSettings;
+})
+

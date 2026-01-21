e7048cb89541509a7c3dabf876591eeb6bad7ced
Bradly Chauke <bradlychauke@gmail.com>
2026-01-21 09:02:17 +0200

    Current functionalities

---
+++ src/main/renderer/src/components/ui/StatusBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+<<<<<<< Updated upstream:src/main/renderer/src/components/ui/StatusBar.jsx
+const StatusBar = () => {
+  return <div></div>;
+};
+
+export default StatusBar;
+=======
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+>>>>>>> Stashed changes:src/renderer/src/App.jsx
